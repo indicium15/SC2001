87720ae625ac6c458ba49109a75291c3302eda9c
indicium15 <67154087+indicium15@users.noreply.github.com>
2022-09-16 11:14:35 +0800

    Added comments for Lab1.java for presentation

--- Lab 1/Lab1.java
+++ Lab 1/Lab1.java
@@ -6,6 +6,12 @@ import java.util.stream.IntStream;
 
 public class Lab1{
     public static int comparisions = 0;
+    /**
+     * Method for performing insertion sort on an input array. Used in the hybridSort() method when the threshold size is reached.
+     * Returns the sorted array.
+     * @param array
+     * @return
+     */
     public static int[] insertionSort(int[] array){
         for(int i = 0; i<array.length;i++){
             int key = i;
@@ -21,6 +27,14 @@ public class Lab1{
         }
         return array;
     }
+
+    /**
+     * Task for Lab1 - implementing hybridSort() by combining the insertionSort and mergeSort algorithms.
+     * Returns the sorted array.
+     * @param temp
+     * @param limitingFactor
+     * @return
+     */
     public static int[] hybridSort(int[] temp, int limitingFactor){
         if(temp.length <= limitingFactor){
             int[] result = insertionSort(temp);
@@ -29,13 +43,13 @@ public class Lab1{
         else{
             int len = temp.length;
             int mid = (int) Math.ceil(len/2);
-            //System.out.printf("Debugging: mid is %d\n",mid);
+            //Finding the mid value and creating two subarrays for the code
             int[] array1 = Arrays.copyOfRange(temp, 0, mid);
             int[] array2 = Arrays.copyOfRange(temp, mid, temp.length);
-            
+            //Recursive HybridSort call on the two subarrays
             array1 = hybridSort(array1, limitingFactor);
             array2 = hybridSort(array2, limitingFactor);
-            
+            //Merging the Sorted Halves
             int[] temparray = new int[len];
             int x = 0;
             int y = 0;
@@ -66,9 +80,19 @@ public class Lab1{
                 z++;
                 y++;
             }
+            //Return sorted array
             return temparray;
         }
     }
+
+    /**
+     * Merge function call that is used for the mergeSort() algorithm below.
+     * Used to compare performance of our hybridSort() against mergeSort().
+     * @param array
+     * @param start
+     * @param mid
+     * @param end
+     */
     public void merge(int[] array, int start, int mid, int end){
         int i,j,k;
         int n1 = mid-start+1;
@@ -108,6 +132,14 @@ public class Lab1{
             k++;
         }
     }
+
+    /**
+     * mergeSort() algorithm following the pseudocode from the SC2001 lecture.
+     * Used to compare the performance of mergeSort() against hybridSort().
+     * @param array
+     * @param start
+     * @param end
+     */
     public void mergeSort(int[]array, int start, int end){
         if(start < end){
             int middle = (start+end) / 2;
@@ -116,11 +148,22 @@ public class Lab1{
             merge(array, start, middle, end);
         }
     }
+
+    /**
+     * Function to print all elements in an array.
+     * @param array
+     */
     public void printArray(int[]array){
         for(int a: array){
             System.out.print(a + " ");
         }
     }
+
+    /**
+     * Function to generate the input data as required to analyze the performance of mergeSort and insertionSort().
+     * Inputs: Max integer "x" and number of arrays to generate. 
+     * @return 
+     */
     public static int[][] generateInputData(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the max integer 'x' for the array:");
@@ -148,53 +191,31 @@ public class Lab1{
         comparisions=0;
     }
     public static void main(String[] args) throws IOException{
-        // Testing the Algorithms
-        // int[] insertionTest = {10,8,9,7,2,3,6,11,63,104};
-        // insertionTest = insertionSort(insertionTest);
-        // for(int i:insertionTest){
-        //     System.out.println(i);
-        // }
-        //Lab1 l1 = new Lab1();
-        //int[] mergeTest = {100,912,81,7,67643,523211,431,32,29097,13};
-        //l1.hybridSort(mergeTest,10);
-        //l1.printArray(mergeTest);
-        
-        //Actual Lab 1
+        //Example Class 1
+        //Generate Input Data
         int[][] inputData = generateInputData();
-        //int[][] compArr = new int[6][5];
-        //Scanner sc = new Scanner(System.in);
 
         //Run Hybrid Sort on Different Sized Datasets with S=10
-        // System.out.println("Hybrid Sort wth Fixed S=10");
-        // int index = 0;
-        // for(int i = 0; i<inputData.length; i++){
-        //     resetCounter();
-        //     long startTime = System.nanoTime();
-        //     hybridSort(inputData[i], 10);
-        //     long stopTime = System.nanoTime();
-        //     double elapsedTime = (stopTime-startTime)/1_000_000_000;
-        //     System.out.printf("Array Size %d | Threshold Size %d | Comparisons %d | Time Taken %d \n",inputData[i].length, 10, comparisions,stopTime-startTime);
-        //     //System.out.printf("Elapsed Time : %f seconds\n", elapsedTime);
-        //     //System.out.printf("Comparisions: %d\n", comparisions);
-        //     compArr[0][index] = comparisions;
-        // }
+        System.out.println("Hybrid Sort wth Fixed S=10");
+        for(int i = 0; i<inputData.length; i++){
+            resetCounter();
+            long startTime = System.nanoTime();
+            hybridSort(inputData[i], 10);
+            long stopTime = System.nanoTime();
+            System.out.printf("Array Size %d | Threshold Size %d | Comparisons %d | Time Taken %d \n",inputData[i].length, 10, comparisions,stopTime-startTime);
+        }
 
         //Run Hybrid Sort on Dataset with N=1,000,000 and various values of S
         System.out.println("Hybrid Sort wth Varied S");
         int sValues[] = IntStream.range(1,101).toArray();
         for(int i = 0; i<inputData.length; i++){
-            int j = 0;
             resetCounter();
             for(int threshold:sValues){
                 resetCounter();
                 long startTime = System.nanoTime();
                 hybridSort(inputData[i], threshold);
                 long stopTime = System.nanoTime();
-                //double elapsedTime = (stopTime-startTime)/1_000_000_000;
                 System.out.printf("Array Size %d | Threshold Size %d | Comparisons %d | Time Taken %d \n",inputData[i].length, threshold, comparisions,stopTime-startTime);
-                //System.out.printf("Elapsed Time : %f seconds\n", elapsedTime);
-                //System.out.printf("Comparisions: %d\n", comparisions);
-                j++;
             }
         }
     }
